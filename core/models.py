075612ae0a2da75881e8d181098991e3ca97f015
Viakol <viakol.lokaiv@gmail.com>
2026-08-12 09:49:36 +0300

    Add core/views.py and core/models.py

---
+++ core/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __clil_str__(self):
+        return self.name
+
+class Task(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    is_done = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, related_name="tasks")
+
+    class Meta:
+        # Orders by not done first, then newest to oldest
+        ordering = ["is_done", "-created_at"]
+
+    def __str__(self):
+        return self.content[:20]
